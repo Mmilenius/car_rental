a66135eee5fefded05b8e3af620c96442b2c4a33
ÐMaksym <Maksym@mail.com>
2025-05-10 19:28:03 +0300

    add main page, about page. added css, js files

--- main/views.py
+++ main/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Home - Головна',
+        'content': 'Прокат автомобілів',
+    }
+
+    return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        'title': 'Home - Про сайт',
+        'content': 'Про сайт',
+        'text_on_page': 'Текст на сторінці',
+    }
+
+    return render(request, 'main/about.html', context)
