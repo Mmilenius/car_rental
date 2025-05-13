6f11985df3e6a0b395e026fa5ff4abd14f2ac626
ÐMaksym <Maksym@mail.com>
2025-05-13 13:57:09 +0300

    add ajax

--- static/deps/js/jquery-ajax.js
+++ static/deps/js/jquery-ajax.js
@@ -1,181 +1,209 @@
-// Когда html документ готов (прорисован)
+// Коли HTML-документ готовий (промальований)
 $(document).ready(function () {
-    // берем в переменную элемент разметки с id jq-notification для оповещений от ajax
+    // Беремо в змінну елемент розмітки з id jq-notification для сповіщень від ajax
     var successMessage = $("#jq-notification");
 
-    // // Ловим собыитие клика по кнопке добавить в корзину
-    // $(document).on("click", ".add-to-cart", function (e) {
-    //     // Блокируем его базовое действие
-    //     e.preventDefault();
+    // Ловимо подію кліку по кнопці "додати в корзину"
+    $(document).on("click", ".add-to-cart", function (e) {
+        // Блокуємо її базову дію
+        e.preventDefault();
 
-    //     // Берем элемент счетчика в значке корзины и берем оттуда значение
-    //     var goodsInCartCount = $("#goods-in-cart-count");
-    //     var cartCount = parseInt(goodsInCartCount.text() || 0);
+        // Беремо елемент лічильника у значку корзини та беремо звідти значення
+        var carsInCartCount = $("#cars-in-cart-count");
+        var cartCount = parseInt(carsInCartCount.text() || 0);
 
-    //     // Получаем id товара из атрибута data-product-id
-    //     var product_id = $(this).data("product-id");
+        // Отримуємо id авто з атрибута data-car-id
+        var car_id = $(this).data("car-id");
 
-    //     // Из атрибута href берем ссылку на контроллер django
-    //     var add_to_cart_url = $(this).attr("href");
+        // З атрибута href беремо посилання на контролер django
+        var add_to_cart_url = $(this).attr("href");
 
-    //     // делаем post запрос через ajax не перезагружая страницу
-    //     $.ajax({
-    //         type: "POST",
-    //         url: add_to_cart_url,
-    //         data: {
-    //             product_id: product_id,
-    //             csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
-    //         },
-    //         success: function (data) {
-    //             // Сообщение
-    //             successMessage.html(data.message);
-    //             successMessage.fadeIn(400);
-    //             // Через 7сек убираем сообщение
-    //             setTimeout(function () {
-    //                 successMessage.fadeOut(400);
-    //             }, 7000);
+        // Виконуємо POST-запит через ajax без перезавантаження сторінки
+        $.ajax({
+            type: "POST",
+            url: add_to_cart_url,
+            data: {
+                car_id: car_id,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+            success: function (data) {
+                // Повідомлення
+                successMessage.html(data.message);
+                successMessage.fadeIn(400);
+                // Через 7 сек ховаємо повідомлення
+                setTimeout(function () {
+                    successMessage.fadeOut(400);
+                }, 7000);
 
-    //             // Увеличиваем количество товаров в корзине (отрисовка в шаблоне)
-    //             cartCount++;
-    //             goodsInCartCount.text(cartCount);
+                // Збільшуємо кількість авто у корзині (в шаблоні)
+                cartCount++;
+                carsInCartCount.text(cartCount);
 
-    //             // Меняем содержимое корзины на ответ от django (новый отрисованный фрагмент разметки корзины)
-    //             var cartItemsContainer = $("#cart-items-container");
-    //             cartItemsContainer.html(data.cart_items_html);
+                // Змінюємо вміст корзини на відповідь від django (новий відмальований фрагмент корзини)
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
 
-    //         },
+            },
+            error: function (data) {
+                console.log("Помилка при додаванні авто в корзину");
+            },
+        });
+    });
 
-    //         error: function (data) {
-    //             console.log("Ошибка при добавлении товара в корзину");
-    //         },
-    //     });
-    // });
+    // Ловимо подію кліку по кнопці "видалити авто з корзини"
+    $(document).on("click", ".remove-from-cart", function (e) {
+        // Блокуємо її базову дію
+        e.preventDefault();
 
+        // Беремо елемент лічильника у значку корзини та беремо звідти значення
+        var carsInCartCount = $("#cars-in-cart-count");
+        var cartCount = parseInt(carsInCartCount.text() || 0);
 
+        // Отримуємо id корзини з атрибута data-cart-id
+        var cart_id = $(this).data("cart-id");
+        // З атрибута href беремо посилання на контролер django
+        var remove_from_cart = $(this).attr("href");
 
+        // Виконуємо POST-запит через ajax без перезавантаження сторінки
+        $.ajax({
+            type: "POST",
+            url: remove_from_cart,
+            data: {
+                cart_id: cart_id,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+            success: function (data) {
+                // Повідомлення
+                successMessage.html(data.message);
+                successMessage.fadeIn(400);
+                // Через 7 сек ховаємо повідомлення
+                setTimeout(function () {
+                    successMessage.fadeOut(400);
+                }, 7000);
 
-    // // Ловим собыитие клика по кнопке удалить товар из корзины
-    // $(document).on("click", ".remove-from-cart", function (e) {
-    //     // Блокируем его базовое действие
-    //     e.preventDefault();
+                // Зменшуємо кількість авто у корзині (в шаблоні)
+                cartCount -= data.quantity_deleted;
+                carsInCartCount.text(cartCount);
 
-    //     // Берем элемент счетчика в значке корзины и берем оттуда значение
-    //     var goodsInCartCount = $("#goods-in-cart-count");
-    //     var cartCount = parseInt(goodsInCartCount.text() || 0);
+                // Змінюємо вміст корзини на відповідь від django (новий відмальований фрагмент корзини)
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
 
-    //     // Получаем id корзины из атрибута data-cart-id
-    //     var cart_id = $(this).data("cart-id");
-    //     // Из атрибута href берем ссылку на контроллер django
-    //     var remove_from_cart = $(this).attr("href");
-    
-    //     // делаем post запрос через ajax не перезагружая страницу
-    //     $.ajax({
+            },
+            error: function (data) {
+                console.log("Помилка при видаленні авто з корзини");
+            },
+        });
+    });
 
-    //         type: "POST",
-    //         url: remove_from_cart,
-    //         data: {
-    //             cart_id: cart_id,
-    //             csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
-    //         },
-    //         success: function (data) {
-    //             // Сообщение
-    //             successMessage.html(data.message);
-    //             successMessage.fadeIn(400);
-    //             // Через 7сек убираем сообщение
-    //             setTimeout(function () {
-    //                 successMessage.fadeOut(400);
-    //             }, 7000);
+    // Обробник події для зменшення кількості
+    $(document).on("click", ".decrement", function () {
+        // Беремо посилання на контролер django з атрибута data-cart-change-url
+        var url = $(this).data("cart-change-url");
+        // Беремо id корзини з атрибута data-cart-id
+        var cartID = $(this).data("cart-id");
+        // Шукаємо найближчий input з кількістю
+        var $input = $(this).closest('.input-group').find('.number');
+        // Отримуємо значення кількості авто
+        var currentValue = parseInt($input.val());
+        // Якщо кількість більше одного — тільки тоді зменшуємо
+        if (currentValue > 1) {
+            $input.val(currentValue - 1);
+            // Запускаємо функцію, визначену нижче
+            updateCart(cartID, currentValue - 1, -1, url);
+        }
+    });
 
-    //             // Уменьшаем количество товаров в корзине (отрисовка)
-    //             cartCount -= data.quantity_deleted;
-    //             goodsInCartCount.text(cartCount);
+    // Обробник події для збільшення кількості
+    $(document).on("click", ".increment", function () {
+        var url = $(this).data("cart-change-url");
+        var cartID = $(this).data("cart-id");
+        var $input = $(this).closest('.input-group').find('.number');
+        var currentValue = parseInt($input.val());
 
-    //             // Меняем содержимое корзины на ответ от django (новый отрисованный фрагмент разметки корзины)
-    //             var cartItemsContainer = $("#cart-items-container");
-    //             cartItemsContainer.html(data.cart_items_html);
+        $input.val(currentValue + 1);
 
-    //         },
+        updateCart(cartID, currentValue + 1, 1, url);
+    });
 
-    //         error: function (data) {
-    //             console.log("Ошибка при добавлении товара в корзину");
-    //         },
-    //     });
-    // });
+    function updateCart(cartID, quantity, change, url) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                cart_id: cartID,
+                quantity: quantity,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+            success: function (data) {
+                successMessage.html(data.message);
+                successMessage.fadeIn(400);
+                setTimeout(function () {
+                    successMessage.fadeOut(400);
+                }, 7000);
 
+                var carsInCartCount = $("#cars-in-cart-count");
+                var cartCount = parseInt(carsInCartCount.text() || 0);
+                cartCount += change;
+                carsInCartCount.text(cartCount);
 
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
+            },
+            error: function (data) {
+                console.log("Помилка при оновленні кількості авто в корзині");
+            },
+        });
+    }
 
+    // Беремо елемент для сповіщень від django
+    var notification = $('#notification');
+    if (notification.length > 0) {
+        setTimeout(function () {
+            notification.alert('close');
+        }, 7000);
+    }
 
-    // // Теперь + - количества товара 
-    // // Обработчик события для уменьшения значения
-    // $(document).on("click", ".decrement", function () {
-    //     // Берем ссылку на контроллер django из атрибута data-cart-change-url
-    //     var url = $(this).data("cart-change-url");
-    //     // Берем id корзины из атрибута data-cart-id
-    //     var cartID = $(this).data("cart-id");
-    //     // Ищем ближайшеий input с количеством 
-    //     var $input = $(this).closest('.input-group').find('.number');
-    //     // Берем значение количества товара
-    //     var currentValue = parseInt($input.val());
-    //     // Если количества больше одного, то только тогда делаем -1
-    //     if (currentValue > 1) {
-    //         $input.val(currentValue - 1);
-    //         // Запускаем функцию определенную ниже
-    //         // с аргументами (id карты, новое количество, количество уменьшилось или прибавилось, url)
-    //         updateCart(cartID, currentValue - 1, -1, url);
-    //     }
-    // });
+    // При кліку по значку корзини відкриваємо модальне вікно
+    $('#modalButton').click(function () {
+        $('#exampleModal').appendTo('body');
+        $('#exampleModal').modal('show');
+    });
 
-    // // Обработчик события для увеличения значения
-    // $(document).on("click", ".increment", function () {
-    //     // Берем ссылку на контроллер django из атрибута data-cart-change-url
-    //     var url = $(this).data("cart-change-url");
-    //     // Берем id корзины из атрибута data-cart-id
-    //     var cartID = $(this).data("cart-id");
-    //     // Ищем ближайшеий input с количеством 
-    //     var $input = $(this).closest('.input-group').find('.number');
-    //     // Берем значение количества товара
-    //     var currentValue = parseInt($input.val());
+    // Подія кліку по кнопці закриття вікна корзини
+    $('#exampleModal .btn-close').click(function () {
+        $('#exampleModal').modal('hide');
+    });
 
-    //     $input.val(currentValue + 1);
+    // Обробник події для вибору способу доставки
+    $("input[name='requires_delivery']").change(function () {
+        var selectedValue = $(this).val();
+        // Показуємо або приховуємо поле введення адреси доставки
+        if (selectedValue === "1") {
+            $("#deliveryAddressField").show();
+        } else {
+            $("#deliveryAddressField").hide();
+        }
+    });
 
-    //     // Запускаем функцию определенную ниже
-    //     // с аргументами (id карты, новое количество, количество уменьшилось или прибавилось, url)
-    //     updateCart(cartID, currentValue + 1, 1, url);
-    // });
+    // Форматування вводу номера телефону у формі (xxx) xxx-хххх
+    document.getElementById('id_phone_number').addEventListener('input', function (e) {
+        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+    });
 
-    // function updateCart(cartID, quantity, change, url) {
-    //     $.ajax({
-    //         type: "POST",
-    //         url: url,
-    //         data: {
-    //             cart_id: cartID,
-    //             quantity: quantity,
-    //             csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
-    //         },
- 
-    //         success: function (data) {
-    //              // Сообщение
-    //             successMessage.html(data.message);
-    //             successMessage.fadeIn(400);
-    //              // Через 7сек убираем сообщение
-    //             setTimeout(function () {
-    //                  successMessage.fadeOut(400);
-    //             }, 7000);
- 
-    //             // Изменяем количество товаров в корзине
-    //             var goodsInCartCount = $("#goods-in-cart-count");
-    //             var cartCount = parseInt(goodsInCartCount.text() || 0);
-    //             cartCount += change;
-    //             goodsInCartCount.text(cartCount);
+    // Перевірка на клієнті правильності номера телефону у формі
+    $('#create_order_form').on('submit', function (event) {
+        var phoneNumber = $('#id_phone_number').val();
+        var regex = /^\(\d{3}\) \d{3}-\d{4}$/;
 
-    //             // Меняем содержимое корзины
-    //             var cartItemsContainer = $("#cart-items-container");
-    //             cartItemsContainer.html(data.cart_items_html);
-
-    //         },
-    //         error: function (data) {
-    //             console.log("Ошибка при добавлении товара в корзину");
-    //         },
-    //     });
-    // }
+        if (!regex.test(phoneNumber)) {
+            $('#phone_number_error').show();
+            event.preventDefault();
+        } else {
+            $('#phone_number_error').hide();
+            var cleanedPhoneNumber = phoneNumber.replace(/[()\-\s]/g, '');
+            $('#id_phone_number').val(cleanedPhoneNumber);
+        }
+    });
 });
