aa7a74fa5f892d6277130c9e235f9bf0ee447ff2
ÐMaksym <Maksym@mail.com>
2025-05-13 15:23:35 +0300

    add ajax for add to cart

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 # Create your views here.
 from cars.models import Cars
 from carts.models import Cart
-def carts_add(request, car_slug):
-    car = Cars.objects.get(slug=car_slug)
+from carts.utils import get_user_carts
+
+
+def carts_add(request):
+
+    car_id = request.POST.get('car_id')
+    car = Cars.objects.get(id=car_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, car=car)
@@ -16,8 +23,16 @@ def carts_add(request, car_slug):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, car=car, period=1)
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
 
-    return redirect(request.META['HTTP_REFERER'])
+    response_data = {
+        "message": "Машина добавленв в корзину",
+        'cart_items_html': cart_items_html,
+    }
+
+    return JsonResponse(response_data)
 def carts_remove(request, cart_id):
 
     cart = Cart.objects.get(id=cart_id)
