d0f803a0ab5975075fc166ce4c8972af1daa8882
ÐMaksym <Maksym@mail.com>
2025-05-13 12:09:24 +0300

    add in cart(first)

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+from users.models import User
+from cars.models import Cars
+
 # Create your models here.
+
+class CartQuerySet(models.QuerySet):
+    def total_price(self):
+        return sum(cart.car_price() for cart in self)
+
+    def total_period(self):
+        if self:
+            return sum(cart.period for cart in self)
+        return 0
+class Cart(models.Model):
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Користувач')
+    car = models.ForeignKey(to=Cars, on_delete=models.CASCADE, verbose_name='Машина')
+    period = models.PositiveSmallIntegerField(default=0, verbose_name='Період')
+    session_key = models.CharField(max_length=32, null=True,blank=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата додавлення машини')
+
+    class Meta:
+        db_table = 'cart'
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзина"
+
+    objects = CartQuerySet.as_manager()
+    def cars_price(self):
+        return round(self.car.price_for_sell() * self.period, 2)
+
+    def __str__(self):
+        if self.user:
+            return f'Корзина {self.user.username} | Машина {self.car.name} | Період {self.period}'
--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-def carts_add(request):
-    ...
+from cars.models import Cars
+from carts.models import Cart
+def carts_add(request, car_slug):
+    car = Cars.objects.get(slug=car_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, car=car)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.period += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, car=car, period=1)
+
+    return redirect(request.META['HTTP_REFERER'])
 def carts_remove(request):
     ...
 def carts_change(request):
