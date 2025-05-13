3d1593c4f7e429d6997345f533e6be62e5025c10
ÐMaksym <Maksym@mail.com>
2025-05-13 20:43:59 +0300

    add craete order

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+from cars.models import Cars
+
+from users.models import User
+
+
+class OrderitemQueryset(models.QuerySet):
+
+    def total_price(self):
+        return sum(cart.cars_price() for cart in self)
+
+    def total_period(self):
+        if self:
+            return sum(cart.period for cart in self)
+        return 0
+
+
+class Order(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT, blank=True, null=True, verbose_name="Користувач",
+                             default=None)
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата створення бронювання")
+    phone_number = models.CharField(max_length=20, verbose_name="Номер телефона")
+    start_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата початку оренди")
+    end_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата зікінченян оренди")
+    requires_delivery = models.BooleanField(default=False, verbose_name="Потрібна доставка")
+    delivery_address = models.TextField(null=True, blank=True, verbose_name="Адреса доставки")
+    payment_on_get = models.BooleanField(default=False, verbose_name="Оплата при отриманні")
+    is_paid = models.BooleanField(default=False, verbose_name="Оплачено")
+    status = models.CharField(max_length=50, default='Опрацьовується', verbose_name="Статус замовлення")
+
+    class Meta:
+        db_table = "order"
+        verbose_name = "Замовлення"
+        verbose_name_plural = "Замовлення"
+        ordering = ("id",)
+
+    def __str__(self):
+        return f"Замовлдення № {self.pk} | Клієнт {self.user.first_name} {self.user.last_name}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE, verbose_name="Замовлення")
+    car = models.ForeignKey(to=Cars, on_delete=models.SET_DEFAULT, null=True, verbose_name="Машина",
+                                default=None)
+    name = models.CharField(max_length=150, verbose_name="Назва")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Ціна")
+    period = models.PositiveIntegerField(default=0, verbose_name="Термін")
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата створення бронювання")
+
+    class Meta:
+        db_table = "order_item"
+        verbose_name = "Орендовану машину"
+        verbose_name_plural = "Орендовані машини"
+        ordering = ("id",)
+
+    objects = OrderitemQueryset.as_manager()
+
+    def cars_price(self):
+        return round(self.cars.price_for_sell() * self.period, 2)
+
+    def __str__(self):
+        return f"Машина {self.name} | Замовлення № {self.order.pk}"
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,74 @@
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect
+
 from django.shortcuts import render
+from django.db import transaction
+from django.contrib import messages
+
+
+from carts.models import Cart
+from orders.models import Order, OrderItem
+from orders.forms import CreateOrderForm
+
+
 
 def create_order(request):
-    return render(request, 'orders/create_order.html')
+    if request.method == 'POST':
+        form = CreateOrderForm(data=request.POST)
+        if form.is_valid():
+            try:
+                with transaction.atomic():
+                    user = request.user
+                    cart_items = Cart.objects.filter(user=user)
+
+                    if cart_items.exists():
+                        # Створити бронювання
+                        order = Order.objects.create(
+                            user=user,
+                            phone_number=form.cleaned_data['phone_number'],
+                            end_date=form.cleaned_data['end_date'],
+                            start_date=form.cleaned_data['start_date'],
+                            requires_delivery=form.cleaned_data['requires_delivery'],
+                            delivery_address=form.cleaned_data['delivery_address'],
+                            payment_on_get=form.cleaned_data['payment_on_get'],
+                        )
+                        # Створити форму аренди авто
+                        for cart_item in cart_items:
+                            car = cart_item.car
+                            name = cart_item.car.name
+                            price = cart_item.car.price_for_sell()
+                            period = cart_item.period
+
+
+                            OrderItem.objects.create(
+                                order=order,
+                                car=car,
+                                name=name,
+                                price=price,
+                                period=period,
+                            )
+
+
+                        # Очистити кошик користувача після створення замовлення
+                        cart_items.delete()
+
+                        messages.success(request, 'Замовлення оформлено!')
+                        return redirect('users:profile')
+            except ValidationError as e:
+                messages.success(request, str(e))
+                return redirect('orders:create_order')
+    else:
+        initial = {
+            'first_name': request.user.first_name,
+            'last_name': request.user.last_name,
+        }
+
+        form = CreateOrderForm(initial=initial)
+
+    context = {
+        'title': 'Головна - Оформлення бронювання',
+        'form': form,
+        'order': True,
+    }
+    return render(request, 'orders/create_order.html', context=context)
+
