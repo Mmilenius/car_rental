e4e4210de9298bab26e13264a78eb077e4dfbea8
ÐMaksym <Maksym@mail.com>
2025-05-13 21:16:59 +0300

    update admin setting

--- orders/admin.py
+++ orders/admin.py
@@ -3,5 +3,66 @@ from django.contrib import admin
 # Register your models here.
 from orders.models import Order, OrderItem
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+class OrderItemTabulareAdmin(admin.TabularInline):
+    model = OrderItem
+    fields = "car", "name", "price", "period"
+    search_fields = (
+        "car",
+        "name",
+    )
+    extra = 0
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = "order", "car", "name", "price", "period"
+    search_fields = (
+        "order",
+        "car",
+        "name",
+    )
+
+
+class OrderTabulareAdmin(admin.TabularInline):
+    model = Order
+    fields = (
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    )
+
+    search_fields = (
+        "requires_delivery",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    )
+    readonly_fields = ("created_timestamp",)
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    )
+
+    search_fields = (
+        "id",
+    )
+    readonly_fields = ("created_timestamp",)
+    list_filter = (
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+    )
+    inlines = (OrderItemTabulareAdmin,)
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,13 @@
 # Register your models here.
 from django.contrib import admin
+from orders.admin import OrderTabulareAdmin
 
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "email", ]
+    search_fields = ["username", "first_name", "last_name", "email", ]
+
+    inlines = [OrderTabulareAdmin]
